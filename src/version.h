b400a2b1cc40261c14017a12a0a8a08357cdbfad
BigTear <54252779+BigTear@users.noreply.github.com>
2021-09-28 22:51:40 +0800

    初始化原版源码

---
+++ src/version.h
@@ -0,0 +1,5 @@
+#define NSSM_VERSION _T("0.0")
+#define NSSM_VERSIONINFO 0,0,0,0
+#define NSSM_DATE _T("2021/09/28 ÖÜ¶þ")
+#define NSSM_FILEFLAGS VS_FF_PRERELEASE
+#define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-")
