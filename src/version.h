e40f1079e25d4fdeb1b109e7013ef9b1e1e0c1dc
BigTear <54252779+BigTear@users.noreply.github.com>
2021-09-29 01:00:30 +0800

    汉化完成

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NSSM_VERSION _T("0.0")
 #define NSSM_VERSIONINFO 0,0,0,0
-#define NSSM_DATE _T("2021/09/28 周二")
+#define NSSM_DATE _T("2021/09/29 周三")
 #define NSSM_FILEFLAGS VS_FF_PRERELEASE
 #define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-")
